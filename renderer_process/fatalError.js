24ea4f25ef33000940239baa7404c830d3b93d8f
Karmac <karmacdesign@gmail.com>
2019-06-28 21:17:07 +0200

    Habilitar el botón de reinicio de la aplicación.

--- renderer_process/fatalError.js
+++ renderer_process/fatalError.js
@@ -1,5 +1,5 @@
 const { ipcRenderer } = require('electron')
-const { getCurrentWindow } = require('electron').remote
+const { getCurrentWindow, app } = require('electron').remote
 
 // Función para llamar al error fatal desde el proceso de renderizado.
 // Se exporta; hay que llamarla usando { triggerFatalError } = require(archivo).
@@ -14,12 +14,22 @@ ipcRenderer.on('triggerFatalError', (event, arg) => {
 
 // Manejar todos los botones de cierre de la aplicación, para no repetir código.
 document.addEventListener('DOMContentLoaded', event => {
-	const buttons = document.querySelectorAll('.js-closeApp')
-	buttons.forEach(button => {
+	const closeButtons = document.querySelectorAll('.js-closeApp')
+	const relauchButtons = document.querySelectorAll('.js-relauchApp')
+
+	closeButtons.forEach(button => {
 		button.addEventListener('click', event => {
 			getCurrentWindow().close()
 		})
 	})
+
+	relauchButtons.forEach(button => {
+		button.addEventListener('click', event => {
+			app.relaunch()
+			app.quit()
+		})
+	})
+
 })
 
 module.exports = { triggerFatalError }
